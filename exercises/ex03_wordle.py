a375c36c265bf1e071262fa85ee7729bfc5ab2ef
Gregory Glasby <gglasby@unc.edu>
2022-09-14 14:28:50 -0400

    ex03 final draft

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -5,7 +5,6 @@ __author__ = "730567386"
 
 def contains_char(word_being_searched: str, single_character: str) -> bool:
     """Shows whether the single character is present in the word being searched or not."""
-
     assert len(single_character) == 1
 
     i: int = 0 
@@ -20,7 +19,6 @@ def contains_char(word_being_searched: str, single_character: str) -> bool:
 
 def emojified(player_guess: str, secret_word: str) -> str:
     """Prints a color coded line of boxes to show the player if they have the correct letters and if they are at the correct index."""
-
     assert len(player_guess) == len(secret_word) 
 
     WHITE_BOX: str = "\U00002B1C"
@@ -32,8 +30,8 @@ def emojified(player_guess: str, secret_word: str) -> str:
     while i < len(player_guess):
         if player_guess[i] == secret_word[i]:
             boxes = boxes + GREEN_BOX
-        elif contains_char(secret_word, player_guess[i]) == True:
-                boxes = boxes + YELLOW_BOX
+        elif contains_char(secret_word, player_guess[i]):
+            boxes = boxes + YELLOW_BOX
         else: 
             boxes = boxes + WHITE_BOX
         
@@ -44,10 +42,34 @@ def emojified(player_guess: str, secret_word: str) -> str:
 
 def input_guess(expected_length: int) -> str:
     """Repeats prompt until the length of the input matches the expected length of the secret word."""
-
     player_guess = input(f"Enter a {expected_length} character word: ")
 
     while expected_length != len(player_guess):
         player_guess = input(f"That wasn't {expected_length} chars! Try again: ")
     
     return player_guess
+
+
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    secret_word: str = "codes"
+    turn_count: int = 1
+    player_guess: str = ""
+
+    while turn_count <= 6 and player_guess != secret_word:
+        print(f"=== Turn {turn_count}/6 ===")
+        player_guess = input_guess(len(secret_word))
+        print(emojified(player_guess, secret_word))
+    
+        turn_count += 1
+
+    if player_guess == secret_word:
+        turn_count -= 1
+        print(f"You won in {turn_count}/6 turns!")
+
+    if turn_count > 6:
+        print("X/6 - Sorry, try again tomorrow!")
+
+
+if __name__ == "__main__":
+    main()
