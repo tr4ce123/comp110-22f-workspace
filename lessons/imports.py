8a9773dcd096bf7da828eefd83a47de38328ae2b
Gregory Glasby <gglasby@unc.edu>
2022-09-20 17:19:28 -0400

    LS15

---
+++ lessons/imports.py
@@ -0,0 +1,18 @@
+"""Examples of importing Python."""
+
+
+from lessons import helpers
+from lessons import helpers as hp
+
+# Import names defined globally in a module
+from lessons.helpers import powerful, THE_ANSWER
+
+def main() -> None:
+    """Entrypoint of program."""
+    print(helpers.powerful(2, 4))
+    print(f"The answer: {helpers.THE_ANSWER}")
+    print(powerful(2, 4))
+    print(THE_ANSWER)
+
+if __name__ == "__main__":
+    main()
