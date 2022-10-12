d1d0e2e5d17004231820a1654d460bbefaf8c950
Gregory Glasby <gglasby@unc.edu>
2022-10-12 11:28:53 -0400

    data

---
+++ lessons/data_utils.py
@@ -0,0 +1,8 @@
+"""Some helpful utility functins for working with CSV files."""
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+    # TODO: More work!
+    return result
