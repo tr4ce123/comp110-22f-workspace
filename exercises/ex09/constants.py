1a5ff8fa2eb8f82684a2bd87cacf2dd65664aed2
Gregory Glasby <gglasby@unc.edu>
2022-11-05 18:07:27 -0400

    ex09 Part 1 Complete

--- exercises/ex09/constants.py
+++ exercises/ex09/constants.py
@@ -11,5 +11,9 @@ MIN_Y: float = -MAX_Y
 VIEW_HEIGHT: int = BOUNDS_HEIGHT + 20
 
 CELL_RADIUS: int = 15
-CELL_COUNT: int = 1
+CELL_COUNT: int = 10
 CELL_SPEED: float = 5.0
+
+VULNERABLE: int = 0
+INFECTED: int = 1
+INI_INFECTED: int = 3
--- exercises/ex09/model.py
+++ exercises/ex09/model.py
@@ -6,7 +6,7 @@ from exercises.ex09 import constants
 from math import sin, cos, pi
 
 
-__author__ = ""  # TODO
+__author__ = "730567386"
 
 
 class Point:
@@ -30,7 +30,7 @@ class Cell:
     """An individual subject in the simulation."""
     location: Point
     direction: Point
-    sickness: int = 0
+    sickness: int = constants.VULNERABLE
 
     def __init__(self, location: Point, direction: Point):
         """Construct a cell with its location and direction."""
@@ -41,10 +41,37 @@ class Cell:
     # Its purpose is to reassign the object's location attribute
     # the result of adding the self object's location with its
     # direction. Hint: Look at the add method.
+    def tick(self) -> None:
+        self.location = self.location.add(self.direction)
         
+    # def color(self) -> str:
+    #     """Return the color representation of a cell."""
+    #     return "black"
+
+    def contract_disease(self) -> None:
+        """Assigns the infected constant to the sickness attribute."""
+        self.sickness = constants.INFECTED
+
+    def is_vulnerable(self) -> bool:
+        """Returns True when the sickness attribute is vulnerable and False if it is not."""
+        if self.sickness == constants.VULNERABLE: 
+            return True
+        else: 
+            return False
+
+    def is_infected(self) -> bool:
+        """Returns True when the sickness attribute is infected and False if it is not."""
+        if self.sickness == constants.INFECTED: 
+            return True 
+        else: 
+            return False
+
     def color(self) -> str:
-        """Return the color representation of a cell."""
-        return "black"
+        """Assigns the cell the color gray if vulnerable, and another color if not."""
+        if self.sickness == constants.VULNERABLE:
+            return "gray"
+        else:
+            return "hot pink"
 
 
 class Model:
@@ -53,27 +80,57 @@ class Model:
     population: list[Cell]
     time: int = 0
 
-    def __init__(self, cells: int, speed: float):
+    def __init__(self, cells: int, speed: float, infected: int):
         """Initialize the cells with random locations and directions."""
+        if infected >= cells or infected <= 0:
+            raise ValueError("Must have less infected cells than total cells or more than 0 infected cells.")
+
         self.population = []
+        for _ in range(cells):
+            start_location: Point = self.random_location()
+            start_direction: Point = self.random_direction(speed)
+            cell: Cell = Cell(start_location, start_direction)
+            self.population.append(cell)
+
+        i: int = 0
+        while i < infected:
+            self.population[i].contract_disease()
+            i += 1
     
     def tick(self) -> None:
         """Update the state of the simulation by one time step."""
         self.time += 1
+        for cell in self.population:
+            cell.tick()
+            self.enforce_bounds(cell)
 
     def random_location(self) -> Point:
         """Generate a random location."""
-        # TODO
-        return Point(0.0, 0.0)
+        start_x: float = random() * constants.BOUNDS_WIDTH - constants.MAX_X
+        start_y: float = random() * constants.BOUNDS_HEIGHT - constants.MAX_Y
+        return Point(start_x, start_y)
 
     def random_direction(self, speed: float) -> Point:
         """Generate a 'point' used as a directional vector."""
-        # TODO
-        return Point(0.0, 0.0)
+        random_angle: float = 2.0 * pi * random()
+        direction_x: float = cos(random_angle) * speed
+        direction_y: float = sin(random_angle) * speed
+        return Point(direction_x, direction_y)
 
     def enforce_bounds(self, cell: Cell) -> None:
         """Cause a cell to 'bounce' if it goes out of bounds."""
-        ...
+        if cell.location.x > constants.MAX_X:
+            cell.location.x = constants.MAX_X
+            cell.direction.x *= -1.0
+        elif cell.location.y > constants.MAX_Y:
+            cell.location.y = constants.MAX_Y
+            cell.direction.y *= -1.0
+        elif cell.location.x < constants.MIN_X:
+            cell.location.x = constants.MIN_X
+            cell.direction.x *= -1.0
+        elif cell.location.y < constants.MIN_Y:
+            cell.location.y = constants.MIN_Y
+            cell.direction.y *= -1.0
 
     def is_complete(self) -> bool:
         """Method to indicate when the simulation is complete."""
