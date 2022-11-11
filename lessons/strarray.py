7b0694d9c8ecbeefc744f1a5fc263d4dfc12691e
Gregory Glasby <gglasby@unc.edu>
2022-11-11 12:15:05 -0500

    Lecture 11/10

---
+++ lessons/strarray.py
@@ -0,0 +1,33 @@
+"""Examples of "vectorized" operations via operator overloading."""
+
+from __future__ import annotations
+from typing import Union
+
+
+class StrArray:
+    items: list[str]
+
+    def __init__(self, items: list[str]):
+        self.items = items
+
+    def __repr__(self) -> str:
+        return f"StrArray({self.items})"
+
+    def __add__(self, rhs: Union[str, StrArray]) -> StrArray:
+        result: StrArray = StrArray([])
+        if isinstance(rhs, str):
+            for item in self.items:
+                result.items.append(item + rhs)
+        else:
+            for i in range(len(self.items)):
+                result.items.append(self.items[i] + rhs.items[i])
+        return result
+
+
+a: StrArray = StrArray(["Armando", "Pete", "Leaky"])
+b: StrArray = StrArray(["Bacot", "Nance", "Black"])
+print(a)
+print(a + "!")
+print(a)
+print(a + b)
+print(b + ", " + a + "!!!")
